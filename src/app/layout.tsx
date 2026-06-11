6fbf6938fc17ab887d2d2b24d6085ca0f7c8de57
santiagosuarez32 <facucde93@gmail.com>
2026-06-11 17:47:41 -0300

    sdfsdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
 import CookieConsent from "@/components/CookieConsent";
+
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -41,6 +43,15 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5FM8TG92');`}
+        </Script>
+        {/* End Google Tag Manager */}
         {/*
           Back/forward navigation serves a server-rendered page that does not
           re-hydrate, leaving client components (e.g. the scroll-aware Navbar)
@@ -72,6 +83,16 @@ export default function RootLayout({
         />
       </head>
       <body className={`${montserrat.className} antialiased bg-black text-white`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5FM8TG92"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll>
           {children}
         </SmoothScroll>
